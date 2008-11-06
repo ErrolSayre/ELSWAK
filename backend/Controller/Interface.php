d413ee54d9a5dab60540c853d8f4eacea278b31a
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2008-11-06 19:38:31 +0000

    Added two methods to controller interface to provide a clear method for easily identifying the uri component that corresponds with a controller as well as the system component the controller manages. The two methods return strings which may or may not be identical.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@31 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- backend/Controller/Interface.php
+++ backend/Controller/Interface.php
@@ -7,5 +7,10 @@
 interface ELSWebAppKit_Controller_Interface
 {
 	public function run(ELSWebAppKit_HTTP_Response $response, array $arguments);
+		// handles the main application function of the controller, taking the response and modifying with with appropriate input from the uri arguments and the request inputs
+	public static function uriComponent();
+		// responds with the uri component that corresponds to the controller (for example int he uri /application/person/id the person component of that uri looks to the user as a directory but corresponds to the person controller of the application
+	public static function systemComponent();
+		// a human (and machine) readible label for the object/class/activity/component that the controller manages (e.g. the person controller manages "persons")
 }
 ?>
