dd5d41eb0fc57b702b30c41dd9aeb646f8414fb9
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2008-11-13 22:07:57 +0000

    Imported test methods from DataGeneral that were forgotten when the file type classes were first migrated.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@34 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

---
+++ testing/File/Type/_misc/CleanupFileNames.php
@@ -0,0 +1,25 @@
+<?php
+// include the data type detector
+require_once('ELSWebAppKit/File/Type/Detector.php');
+
+// read the file name from the post data
+$dirtyName = '';
+$cleanName = '';
+if (isset($_POST['fileName']))
+{
+	$dirtyName = $_POST['fileName'];
+	
+	// clean up nasty characters in the name
+	$cleanName = preg_replace('/[^a-zA-Z0-9\.]/i', '_', $dirtyName);
+}
+
+// output the form
+echo '<form method="post" action="'.$_SERVER['PHP_SELF'].'">'.LF;
+echo '<input type="text" name="fileName" value="'.$dirtyName.'">'.LF;
+echo '<input type="submit" value="Go">'.LF;
+echo '</form>'.LF;
+
+// output the previous name and results
+echo '<p><b>Submitted Name:</b> '.$dirtyName.' ('.ELSWebAppKit_File_Type_Detector::typeFromName($dirtyName).')</p>'.LF;
+echo '<p><b>Cleaned Name:</b> '.$cleanName.' ('.ELSWebAppKit_File_Type_Detector::typeFromName($cleanName).')</p>'.LF;
+?>
