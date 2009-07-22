0f74e0e8cb3acc4f173393386506eec4cccea184
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2009-07-22 23:24:02 +0000

    Fixed setPropertyAsTimestamp to recognized 0000-00-00, 00:00:00, etc as 0 rather than -MAXINT.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@68 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- backend/Settable.php
+++ backend/Settable.php
@@ -243,6 +243,8 @@ class ELSWebAppKit_Settable {
 	protected function _setPropertyAsTimestamp($property, $value) {
 		if (is_numeric($value)) {
 			$this->{$property} = intval($value);
+		} else if (str_replace(array('/', '-', ' '), '', $value) == 0) {
+			$this->{$property} = 0;
 		} else {
 			$this->{$property} = strtotime($value);
 		}
