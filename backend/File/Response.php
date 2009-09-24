3372b5a8eb2fcf46a6ae1e2bcdccf62972b0bd37
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2009-09-24 21:13:27 +0000

    Fixed addContent to alias to setFile. Updated source formatting.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@78 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- backend/File/Response.php
+++ backend/File/Response.php
@@ -4,16 +4,14 @@
 	
 	This class handles responses which read files on the local filesystem to the response.
 */
-require_once('ELSWebAppKit/HTTP/Response.php');
-require_once('ELSWebAppKit/File/Type/Detector.php');
+require_once 'ELSWebAppKit/HTTP/Response.php';
+require_once 'ELSWebAppKit/File/Type/Detector.php';
 class ELSWebAppKit_File_Response
-	extends ELSWebAppKit_HTTP_Response
-{
+	extends ELSWebAppKit_HTTP_Response {
 	protected $fileName;
 	protected $filePath;
 	
-	public function __construct($filePath = null, $fileName = null, $download = true)
-	{
+	public function __construct($filePath = null, $fileName = null, $download = true) {
 		parent::__construct();
 		
 		$this->setHeader('Content-Transfer-Encoding', 'binary', true);
@@ -29,32 +27,27 @@ class ELSWebAppKit_File_Response
 		else
 			$this->setInline();
 	}
-	public function setInline()
-	{
+	public function setInline() {
 		$this->setHeader('Content-Disposition', 'inline', true);
 		return $this;
 	}
-	public function setDownload()
-	{
+	public function setDownload() {
 		if ($this->fileName != false)
 			$this->setHeader('Content-Disposition', 'attachment; filename='.$this->fileName, true);
 		else
 			$this->setHeader('Content-Disposition', 'attachment', true);
 		return $this;
 	}
-	public function setFile($filePath)
-	{
-		if (is_readable($filePath))
-		{
+	public function setFile($filePath) {
+		if (is_readable($filePath)) {
 			$this->filePath = $filePath;
 			$this->setHeader('Content-Type', ELSWebAppKit_File_Type_Detector::typeFromName($this->filePath), true);
-		}
-		else
+			$this->setHeader('Content-Length', filesize($this->filePath), true);
+		} else
 			throw new Exception('Unable to set file path. Please specify a valid file on the local file system.');
 
 	}
-	public function setFileName($fileName = null)
-	{
+	public function setFileName($fileName = null) {
 		if (is_string($fileName))
 			$this->fileName = $fileName;
 		else if (is_readable($this->filePath))
@@ -64,28 +57,21 @@ class ELSWebAppKit_File_Response
 		
 		return $this->setDownload();
 	}
-	public function addContent($content)
-	{
+	public function addContent($content) {
+		return $this->setFile($content);
+	}
+	public function addMessage($message) {
 		return $this;
 	}
-	public function addMessage($message)
-	{
-		return $this;
-	}
-	public function content()
-	{
+	public function content() {
 		if (is_readable($this->filePath))
 			return file_get_contents($this->filePath);
 		return false;
 	}
-	public function sendContent()
-	{
-		if (is_readable($this->filePath))
-		{
-			$this->setHeader('Content-Length', filesize($this->filePath), true);
-			readfile($this->filePath);
+	public function sendContent() {
+		if (is_readable($this->filePath)) {
+			$outputBytes = readfile($this->filePath);
 		}
 		return $this;
 	}
 }
-?>
