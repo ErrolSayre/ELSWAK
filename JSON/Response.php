416f623a6468a037a1b69d5dcdd6346555f30969
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2010-03-02 22:48:18 +0000

    Removed vestigial translator. Modified response to store items as JSON. Added support for JSON that includes function literals (breaks many JSON parsers however). Added further tests. Reformatted.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@114 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- JSON/Response.php
+++ JSON/Response.php
@@ -1,45 +1,65 @@
 <?php
-require_once('ELSWebAppKit/HTTP/Response.php');
-require_once('ELSWebAppKit/JSON/Translator.php');
+require_once 'ELSWebAppKit/HTTP/Response.php';
 class ELSWebAppKit_JSON_Response
-	extends ELSWebAppKit_HTTP_Response
-{
-	public function __construct()
-	{
+	extends ELSWebAppKit_HTTP_Response {
+	public function __construct() {
 		$this->setContentType('text/javascript');
 		$this->setStatus('OK');
 	}
-	protected function filterContentByType($content, $type)
-	{
-		if (is_string($content) && strtolower($type) == 'json')
-			return json_decode($content, true);
-		return $content;
+	protected function filterContentByType($content, $type) {
+		// determine if the content is a string
+		if (is_string($content)) {
+			// determine the supplied type
+			$type = strtolower($type);
+			if ($type == 'json+functions') {
+				return $content;
+			} else if ($type == 'json') {
+				// determine if the content is valid JSON
+				if (json_decode($content, true) !== null) {
+					return $content;
+				}
+			}
+		}
+		// since the content is not a string or is not an acceptable JSON string, encode it
+		return json_encode($content);
 	}
-	public function content()
-	{
+	public function content() {
 		// construct the json representation of this response
 		$json = '';
 		$json .= '"status":"'.$this->status.'",';
 		if (count($this->messages))
-			$json .= '"messages":'.ELSWebAppKit_JSON_Translator::encode($this->messages).',';
+			$json .= '"messages":'.json_encode($this->messages).',';
 		else
 			$json .= '"messages":null,';
-		if (count($this->body))
-			$json .= '"body":'.ELSWebAppKit_JSON_Translator::encode($this->body);
-		else
+		if (count($this->body)) {
+			$body = '';
+			$unnamed = array();
+			foreach ($this->body as $key => $value) {
+				if (is_integer($key)) {
+					$unnamed[] = $value;
+				} else {
+					$body .= '"'.$key.'":'.$value.',';
+				}
+			}
+			if (count($unnamed) > 0) {
+				$body .= '"unnamed":[';
+				foreach ($unnamed as $value) {
+					$body .= $value.',';
+				}
+				$body .= '],';
+			}
+			$json .= '"body":{'.substr($body, 0, -1).'}';
+		} else {
 			$json .= '"body":null';
-		
+		}
 		// return the finished json
 		return '{'.$json.'}';
 	}
-	public function sendCustomHeaders()
-	{
+	public function sendCustomHeaders() {
 		// override this method since the custom headers are included in the JSON response
 		return $this;
 	}
-	public function __toString()
-	{
+	public function __toString() {
 		return $this->content();
 	}
 }
-?>
--- _Testing/JSON/Response.php
+++ _Testing/JSON/Response.php
@@ -1,11 +1,15 @@
 <?php
-require_once('ELSWebAppKit/JSON/Response.php');
-$response = new ELSWebAppKit_JSON_Response();
+require_once 'ELSWebAppKit/JSON/Response.php';
+$response = new ELSWebAppKit_JSON_Response;
 $response->addContent('item 1');
 $response->addContent('item 2');
 $response->addContent('first', 'firstName');
 $response->addContent('last', 'lastName');
 $response->addContent('{"firstName":"person","lastName":"1"}', 'json string 1');
-$response->addContent('{"firstName":"person","lastName":"2"}', null, 'json');
+$response->addContent('{"firstName":"person","lastName":"2"}', 'json literal 1', 'json');
+$response->addContent(array('first' => 'Errol', 'last' => 'Sayre'), 'myself');
+$item = new stdClass;
+$item->date = time();
+$item->name = 'new item';
+$response->addContent($item);
 $response->send();
-?>
