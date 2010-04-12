4613319ca4b10b566300e13168cbf19012faccce
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2010-04-12 20:51:54 +0000

    Added method to ease adding conditionals to queries. Reformatted.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@127 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- MySQL/Conditional/Group.php
+++ MySQL/Conditional/Group.php
@@ -5,113 +5,90 @@
 	This class lumps MySQL conditional statements together into one group using
 	a specified conjunction (AND or OR).
 */
-require_once('ELSWAK/MySQL/Conditional.php');
-require_once('ELSWAK/MySQL/Conjunction.php');
+require_once 'ELSWAK/MySQL/Conditional.php';
+require_once 'ELSWAK/MySQL/Conjunction.php';
+
 class ELSWAK_MySQL_Conditional_Group
-	implements ELSWAK_MySQL_Expression
-{
+	implements ELSWAK_MySQL_Expression {
 	protected $conditions;
 	protected $conjunction;
 	
-	public function __construct(array $conditions = null, ELSWAK_MySQL_Conjunction $conjunction = null)
-	{
-		$this->setConditions
-		(
-			($conditions !== null)?
+	public function __construct(array $conditions = null, ELSWAK_MySQL_Conjunction $conjunction = null) {
+		$this->setConditions ( ($conditions !== null)?
 				$conditions:
 				array()
 		);
-		$this->setConjunction
-		(
-			($conjunction !== null)?
+		$this->setConjunction ( ($conjunction !== null)?
 				$conjunction:
 				new ELSWAK_MySQL_Conjunction()
 		);
 	}
-	public function conditionForKey($index)
-	{
-		if (isset($this->conditions[$index]))
-		{
+	public function conditionForKey($index) {
+		if (isset($this->conditions[$index])) {
 			return $this->conditions[$index];
-		}
-		else
-		{
+		} else {
 			throw new Exception('Invalid key: Condition not found');
 		}
 	}
-	public function addCondition(ELSWAK_MySQL_Expression $condition)
-	{
+	public function addCondition(ELSWAK_MySQL_Expression $condition) {
 		$this->conditions[] = $condition;
 		return $this->conditions[count($this->conditions) - 1];
 	}
-	public function removeConditionForKey($index)
-	{
-		if (isset($this->conditions[$index]))
-		{
+	public function newCondition(ELSWAK_MySQL_Expression $leftSide, ELSWAK_MySQL_Operator $operator = null, ELSWAK_MySQL_Expression $rightSide = null) {
+		$conditional = new ELSWAK_MySQL_Conditional($leftSide, $operator, $rightSide);
+		$this->addCondition($conditional);
+		return $conditional;
+	}
+	public function removeConditionForKey($index) {
+		if (isset($this->conditions[$index])) {
 			array_splice($this->conditions, $index, 1);
-		}
-		else
-		{
+		} else {
 			throw new Exception('Invalid key: Condition not removed');
 		}
 	}
-	public function conditionCount()
-	{
+	public function conditionCount() {
 		return count($this->conditions);
 	}
-	public function conditionKeys()
-	{
+	public function conditionKeys() {
 		return array_keys($this->conditions);
 	}
-	public function conditions()
-	{
+	public function conditions() {
 		return $this->conditions;
 	}
-	public function setConditions(array $conditions)
-	{
+	public function setConditions(array $conditions) {
 		$this->conditions = array();
 		
-		foreach ($conditions as $condition)
-		{
-			if ($condition instanceOf ELSWAK_MySQL_Expression)
-			{
+		foreach ($conditions as $condition) {
+			if ($condition instanceOf ELSWAK_MySQL_Expression) {
 				$this->addCondition($condition);
 			}
 		}
 		return $this;
 	}
-	public function conjunction()
-	{
+	public function conjunction() {
 		return $this->conjunction;
 	}
-	public function setConjunction(ELSWAK_MySQL_Conjunction $conjunction)
-	{
+	public function setConjunction(ELSWAK_MySQL_Conjunction $conjunction) {
 		$this->conjunction = $conjunction;
 		return $this;
 	}
-	public function sql($format = '', $indent = '')
-	{
+	public function sql($format = '', $indent = '') {
 		// set up the sql
 		$sql = '';
 		
 		// determine if we have one condition
 		$conditionCount = count($this->conditions);
-		if ($conditionCount == 1)
-		{
+		if ($conditionCount == 1) {
 			return $indent.$this->conditions[0]->sql($format, $indent.'  ');
-		}
-		else if ($conditionCount > 1)
-		{
+		} else if ($conditionCount > 1) {
 			// assemble the conditions together as a group
 			$sql .= '('.LF;
 			
 			// now process each condition and add it's sql
-			for ($index = 0; $index < $conditionCount; $index++)
-			{
+			for ($index = 0; $index < $conditionCount; $index++) {
 				$sql .= $indent.'  '.$this->conditions[$index]->sql($format, $indent.'  ');
 				
-				if ($index < ($conditionCount - 1))
-				{
+				if ($index < ($conditionCount - 1)) {
 					$sql .= ' '.$this->conjunction->sql($format, $indent.'  ');
 				}
 				
