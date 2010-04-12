75c6cd38303078ce862396ba18894ec5028bfba4
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2010-04-12 20:51:27 +0000

    Reformatted.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@126 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- MySQL/Conditional.php
+++ MySQL/Conditional.php
@@ -6,22 +6,20 @@
 	such as TRUE, FALSE, table.field = table.file, table.field < value, value !=
 	table.field, etc.
 */
-require_once('ELSWAK/MySQL/Field.php');
-require_once('ELSWAK/MySQL/Boolean.php');
-require_once('ELSWAK/MySQL/Field.php');
-require_once('ELSWAK/MySQL/Literal.php');
-require_once('ELSWAK/MySQL/Operator.php');
-require_once('ELSWAK/MySQL/String.php');
+require_once 'ELSWAK/MySQL/Field.php';
+require_once 'ELSWAK/MySQL/Boolean.php';
+require_once 'ELSWAK/MySQL/Field.php';
+require_once 'ELSWAK/MySQL/Literal.php';
+require_once 'ELSWAK/MySQL/Operator.php';
+require_once 'ELSWAK/MySQL/String.php';
 
 class ELSWAK_MySQL_Conditional
-	implements ELSWAK_MySQL_Expression
-{
+	implements ELSWAK_MySQL_Expression {
 	protected $leftSide;
 	protected $operator;
 	protected $rightSide;
 	
-	public function __construct(ELSWAK_MySQL_Expression $leftSide, ELSWAK_MySQL_Operator $operator = null, ELSWAK_MySQL_Expression $rightSide = null)
-	{
+	public function __construct(ELSWAK_MySQL_Expression $leftSide, ELSWAK_MySQL_Operator $operator = null, ELSWAK_MySQL_Expression $rightSide = null) {
 		// setup default values
 		$this->leftSide = null;
 		$this->operator = null;
@@ -31,53 +29,43 @@ class ELSWAK_MySQL_Conditional
 		$this->setOperator($operator);
 		$this->setRightSide($rightSide);
 	}
-	public function leftSide()
-	{
+	public function leftSide() {
 		return $this->leftSide;
 	}
-	public function setLeftSide(ELSWAK_MySQL_Expression $leftSide)
-	{
+	public function setLeftSide(ELSWAK_MySQL_Expression $leftSide) {
 		$this->leftSide = $leftSide;
 		return $this;
 	}
-	public function operator()
-	{
+	public function operator() {
 		return $this->operator;
 	}
-	public function setOperator(ELSWAK_MySQL_Operator $operator)
-	{
+	public function setOperator(ELSWAK_MySQL_Operator $operator) {
 		$this->operator = $operator;
 		return $this;
 	}
-	public function rightSide()
-	{
+	public function rightSide() {
 		return $this->rightSide;
 	}
-	public function setRightSide(ELSWAK_MySQL_Expression $rightSide)
-	{
+	public function setRightSide(ELSWAK_MySQL_Expression $rightSide) {
 		$this->rightSide = $rightSide;
 		return $this;
 	}
-	public function sql($format = 'table.field', $indent = '')
-	{
+	public function sql($format = 'table.field', $indent = '') {
 		// build the sql for this condition
 		$sql = '';
 		
 		// add the left side
-		if ($this->leftSide !== null)
-		{
+		if ($this->leftSide !== null) {
 			$sql .= $this->leftSide->sql($format, $indent);
 		}
 		
 		// add the operator
-		if ($this->operator !== null)
-		{
+		if ($this->operator !== null) {
 			$sql .= ' '.$this->operator->sql($format, $indent);
 		}
 		
 		// add the right side
-		if ($this->rightSide !== null)
-		{
+		if ($this->rightSide !== null) {
 			$sql .= ' '.$this->rightSide->sql($format, $indent);
 		}
 		
