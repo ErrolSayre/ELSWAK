e8606b59ef03a551b3c26c2ee4ea9b44b154ffdc
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2010-11-12 22:56:00 +0000

    Moved form reader to handler to provide one centralized repository for HTML methods so that subclassing handlers as well as reader/writers can benefit readily.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@148 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- HTML/Handler.php
+++ HTML/Handler.php
@@ -1,10 +1,42 @@
 <?php
 /*
-	ELSWAK HTML Form Reader
+	ELSWAK HTML Handler
 	
-	This class provides some methods that have emerged as a recurring pattern in "form reader" classes.
+	This class provides a standard library of html display, and form read/write methods.
 */
-class ELSWAK_HTML_FormReader {
+class ELSWAK_HTML_Handler {
+// =================== 
+// !Display Methods   
+// =================== 
+	public static function navigationBar(ELSWAK_HTML_Response $response, ELSWAK_ManagedObject $record, array $viewTasks = null, array $editTasks = null, array $existenceTasks = null, array $availableTasks = null, $currentTask = null, array $attributes = null) {
+		// setup a value shortcut
+		$modelClass = strtolower($record->modelLabel);
+		
+		// create the container
+		$attributes = $response->addClassToAttributesArray($modelClass.' navigation bar', $attributes);
+		$container = $response->createDiv($response->createElement('h1', $record->label), $attributes);
+		
+		// process the various tasks to create a listing
+		$list = $container->appendChild($response->createUl(null, array('class' => $modelClass.' task container')));
+		
+		// add each section as a subordinate list
+		$sections = array('view', 'edit', 'existence');
+		foreach ($sections as $section) {
+			$sectionList = $response->createUl(null, array('class' => $modelClass.' '.$section.' tasks'));
+			foreach (${$section.'Tasks'} as $task => $taskData) {
+				if (array_key_exists($task, $availableTasks) && $availableTasks[$task]) {
+					$sectionList->appendChild($response->createLi($response->createLink($record->uri.'/'.$task, $taskData['label'], array('title' => $taskData['description'])), $currentTask == $task? array('class' => 'active'): null));
+				}
+			}
+			if ($sectionList->hasChildNodes()) {
+				$list->appendChild($response->createElement('li', $sectionList));
+			}
+		}
+		return $container;
+	}
+	
+	
+	
 // ========================== 
 // !Form Control Detection   
 // ========================== 
