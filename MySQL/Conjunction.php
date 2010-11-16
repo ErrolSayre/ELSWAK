7ea1ea09cc2ec11147ddd207adf6edf0d1b1cdc7
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2010-11-16 22:47:29 +0000

    Modified to use settable. Reformatted.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@165 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- MySQL/Conjunction.php
+++ MySQL/Conjunction.php
@@ -2,33 +2,24 @@
 /*
 	ELSWAK MySQL Conjunction
 */
-class ELSWAK_MySQL_Conjunction
-{
+class ELSWAK_MySQL_Conjunction {
 	protected $conjunction;
 	
-	public function __construct($conjunction = 'AND')
-	{
+	public function __construct($conjunction = 'AND') {
 		$this->setConjunction($conjunction);
 	}
-	public function conjunction()
-	{
+	public function conjunction() {
 		return $this->conjunction;
 	}
-	public function setConjunction($conjunction)
-	{
-		if (strtoupper($conjunction) == 'OR')
-		{
+	public function setConjunction($conjunction) {
+		if (strtoupper($conjunction) == 'OR') {
 			$this->conjunction = 'OR';
-		}
-		else
-		{
+		} else {
 			$this->conjunction = 'AND';
 		}
 		return $this;
 	}
-	public function sql($format = '', $indent = '')
-	{
+	public function sql($format = '', $indent = '') {
 		return $this->conjunction;
 	}
 }
-?>
