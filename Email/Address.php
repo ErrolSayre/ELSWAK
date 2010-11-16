24a48900ae52a0fcb5172017bb4100f35c5afc0b
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2010-11-16 22:16:51 +0000

    Modified to use validator interface.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@162 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- Email/Address.php
+++ Email/Address.php
@@ -3,55 +3,44 @@
 	ELSWAK Email Address
 */
 class ELSWAK_Email_Address
-{
+	extends ELSWAK_Settable
+	implements ELSWAK_Validator_Interface {
 	const EMAIL_ADDRESS_REGX_BASE = '/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i';
 	const EMAIL_ADDRESS_REGX = '/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i';
 	
 	protected $address;
 	
-	public function __construct($address = null)
-	{
+	public function __construct($address = null) {
 		// set the address
 		$this->setAddress($address);
 	}
-	public function address()
-	{
+	public function address() {
 		return $this->address;
 	}
-	public function setAddress($address)
-	{
+	public function setAddress($address) {
 		// verify that this is a valid email address
-		if ($address !== null)
-		{
-			if ($this->verify($address))
-			{
+		if ($address !== null) {
+			if ($this->validate($address)) {
 				// this is a validly formatted email address
 				$this->address = $address;
-			}
-			else
-			{
+			} else {
 				// this is an invalid email address
 				// leave the current value alone
 				throw new Exception('Invalid Email Address: String does not match email address specifications.');
 			}
-		}
-		else
-		{
+		} else {
 			// set the address to null
 			$this->address = null;
 		}
 		return $this;
 	}
-	public static function verify($address)
-	{
-		if (preg_match(self::EMAIL_ADDRESS_REGX, $address))
-		{
+	public static function validate($address) {
+		if (preg_match(self::EMAIL_ADDRESS_REGX, $address)) {
 			return true;
 		}
 		return false;
 	}
-	public function __toString()
-	{
+	public function __toString() {
 		return $this->address;
 	}
 }
