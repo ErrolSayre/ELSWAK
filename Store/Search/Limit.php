0c0c092bc98ad23035b882feeda802f28331e53f
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2011-01-24 22:06:30 +0000

    Reformatted. Added Settable as an ancestor.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@178 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- Store/Search/Limit.php
+++ Store/Search/Limit.php
@@ -3,30 +3,19 @@
 	ELSWAK Store Search Limit
 */
 class ELSWAK_Store_Search_Limit
-{
+	extends ELSWAK_Settable {
 	protected $count;
 	protected $offset;
 	
-	public function __construct($count = 0, $offset = 0)
-	{
+	public function __construct($count = 0, $offset = 0) {
 		$this->setCount($count);
 		$this->setOffset($offset);
 	}
-	public function count()
-	{
-		return $this->count;
-	}
-	public function setCount($count)
-	{
+	public function setCount($count) {
 		$this->count = intval($count);
 		return $this;
 	}
-	public function offset()
-	{
-		return $this->offset;
-	}
-	public function setOffset($offset)
-	{
+	public function setOffset($offset) {
 		$this->offset = intval($offset);
 		return $this;
 	}
