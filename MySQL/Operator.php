aaec1e198f5f70bb847cd2bb2c5610872cca022a
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2011-01-24 22:40:33 +0000

    Reformatted.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@185 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- MySQL/Operator.php
+++ MySQL/Operator.php
@@ -5,82 +5,50 @@
 	Operators are an integral part of a MySQL expression. Operators can have any
 	value that is deemed a valid MySQL operator.
 */
-require_once('ELSWAK/MySQL/Literal.php');
+class ELSWAK_MySQL_Operator_Exception extends ELSWAK_MySQL_Exception {}
+
 class ELSWAK_MySQL_Operator
-{
+	extends ELSWAK_Settable {
 	protected $operator;
 	
-	public function __construct($operator = '=')
-	{
+	public function __construct($operator = '=') {
 		$this->setOperator($operator);
 	}
-	public function operator()
-	{
-		return $this->operator;
-	}
-	public function setOperator($operator)
-	{
+	public function setOperator($operator) {
 		// set up a test value for string values
 		$testValue = strtoupper($operator);
 		
 		// only allow the operator to be a valid operator
-		if ($operator == '=')
-		{
+		if ($operator == '=') {
 			$this->operator = '=';
-		}
-		else if ($operator == '<')
-		{
+		} else if ($operator == '<') {
 			$this->operator = '<';
-		}
-		else if ($operator == '<=')
-		{
+		} else if ($operator == '<=') {
 			$this->operator = '<=';
-		}
-		else if ($operator == '=>')
-		{
+		} else if ($operator == '=>') {
 			$this->operator = '=>';
-		}
-		else if ($operator == '>')
-		{
+		} else if ($operator == '>') {
 			$this->operator = '>';
-		}
-		else if ($operator == '!=')
-		{
+		} else if ($operator == '!=') {
 			$this->operator = '!=';
-		}
-		else if ($testValue == 'IS')
-		{
+		} else if ($testValue == 'IS') {
 			$this->operator = 'IS';
-		}
-		else if ($testValue == 'IS NOT')
-		{
+		} else if ($testValue == 'IS NOT') {
 			$this->operator = 'IS NOT';
-		}
-		else if ($testValue == 'LIKE')
-		{
+		} else if ($testValue == 'LIKE') {
 			$this->operator = 'LIKE';
-		}
-		else if ($testValue == 'NOT LIKE')
-		{
+		} else if ($testValue == 'NOT LIKE') {
 			$this->operator = 'NOT LIKE';
-		}
-		else if ($operator == '<=>')
-		{
+		} else if ($operator == '<=>') {
 			$this->operator = '<=>';
-		}
-		else if ($operator == '<>')
-		{
+		} else if ($operator == '<>') {
 			$this->operator = '<>';
-		}
-		else
-		{
-			$this->operator = '=';
+		} else {
+			return new ELSWAK_MySQL_Operator_Exception('Unable to set operator. Input does not match any MySQL operator.');
 		}
 		return $this;
 	}
-	public function sql($format = '', $indent = '')
-	{
+	public function sql($format = '', $indent = '') {
 		return $this->operator();
 	}
 }
-?>
