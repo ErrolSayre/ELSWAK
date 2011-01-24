428e3f5221f0d9f8a40c12409a33247ee282d843
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2011-01-24 22:02:09 +0000

    Reformatted. Corrected reference to class tester.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@174 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- _Testing/MySQL/Field.php
+++ _Testing/MySQL/Field.php
@@ -1,58 +1,45 @@
 <?php
 // test the Database Field
-include('ELSWAK/MySQL/Table.php');
 $className = 'ELSWAK_MySQL_Field';
 $classPath = 'ELSWAK/MySQL/Field.php';
 
 // set test data
-$classData = array
-(
-	array
-	(
-		'constructor parameters' => array
-		(
+$classData = array (
+	array (
+		'constructor parameters' => array (
 			'name' => 'proposal_account',
 			'table' => new ELSWAK_MySQL_Table('table_name', new ELSWAK_MySQL_Database('TheDatabase')),
 			'mysqlType' => 'int(13) unsigned'
 		),
-		'additional methods' => array
-		(
+		'additional methods' => array (
 			'sql' => null
 		)
 	),
-	array
-	(
-		'constructor parameters' => array
-		(
+	array (
+		'constructor parameters' => array (
 			'name' => 'proposal_number',
 			'table' => new ELSWAK_MySQL_Table('table_name', new ELSWAK_MySQL_Database('TheDatabase')),
 			'mysqlType' => 'int(4) unsigne'
 		),
-		'additional methods' => array
-		(
+		'additional methods' => array (
 			'sql' => null
 		)
 	),
-	array
-	(
-		'constructor parameters' => array
-		(
+	array (
+		'constructor parameters' => array (
 			'name' => 'mark_down_goodnews',
 			'table' => new ELSWAK_MySQL_Table('prices', new ELSWAK_MySQL_Database('Store')),
 			'mysqlType' => 'enum(\'yes\',\'no\')'
 		),
-		'additional methods' => array
-		(
+		'additional methods' => array (
 			'sql' => null
 		)
 	),
 );
-$classMethods = array
-(
+$classMethods = array (
 	'prettyName' => null,
 	'name' => null,
 	'sql' => 'database.table.field',
 );
 // include the class tester
-include('Area51/ClassTester.php');
-?>
+include 'ELSWAK/Utilities/ClassTester.php';
