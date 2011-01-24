2f2914bf8185d1a5dd636cc9cc821f2eda76079f
errol.sayre <errol.sayre@ae5f3247-1a45-0410-a72d-a72f8f2cc9d5>
2011-01-24 22:12:34 +0000

    Reformatted. Removed explicit require/include statements.
    git-svn-id: https://elswebappkit.googlecode.com/svn/trunk@179 ae5f3247-1a45-0410-a72d-a72f8f2cc9d5

--- MySQL/Literal.php
+++ MySQL/Literal.php
@@ -5,32 +5,26 @@
 	A MySQL literal is an expression made up of a specific value. It also allows
 	an hard coded string to be appended to a dynamic query.
 */
-require_once('ELSWAK/MySQL/Expression.php');
+class ELSWAK_MySQL_Literal_Exception extends ELSWAK_MySQL_Exception {}
+
 class ELSWAK_MySQL_Literal
-	implements ELSWAK_MySQL_Expression
-{
+	extends ELSWAK_Settable
+	implements ELSWAK_MySQL_Expression {
 	protected $literal;
 	
-	public function __construct($literal = '')
-	{
+	public function __construct($literal = '') {
 		$this->setLiteral($literal);
 	}
-	public function literal()
-	{
-		return $this->literal;
-	}
-	public function setLiteral($literal)
-	{
+	public function setLiteral($literal) {
 		// ensure that the literal contains only the allowable characters
-		if (preg_match('/^[\w\+\-\.\(\)]*$/', $literal) === 1)
-		{
+		if (preg_match('/^[\w\+\-\.\(\)]*$/', $literal) === 1) {
 			$this->literal = $literal;
+		} else {
+			throw new ELSWAK_MySQL_Literal_Exception('Unable to set literal. Literal does not fit the character set constraints.');
 		}
 		return $this;
 	}
-	public function sql($format = '', $indent = '')
-	{
+	public function sql($format = '', $indent = '') {
 		return $this->literal;
 	}
 }
-?>
