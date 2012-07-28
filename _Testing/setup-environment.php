d76ee692626b5ee09a34f5bcd2d5ea6c8d87d87d
Errol Sayre <errolsayre@mac.com>
2012-07-28 14:52:21 -0500

    Add script to setup basic test environment

---
+++ _Testing/setup-environment.php
@@ -0,0 +1,7 @@
+<?php
+// locate the path to the "include path"
+$path = pathinfo(pathinfo(pathinfo(__FILE__, PATHINFO_DIRNAME), PATHINFO_DIRNAME), PATHINFO_DIRNAME);
+
+// setup the auto-loader
+require_once $path.'/ELSWAK/ClassLoader.php';
+$cal = new ELSWAK_ClassLoader(false, $path);
