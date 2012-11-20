763d681ec1a50fc01bf5f7706d2be40be0e13e70
Errol Sayre <esayre@olemiss.edu>
2012-11-20 17:12:29 -0600

    Make testing environment more portable.
    Require statements now use a derived absolute path rather than a
    relative path.

--- _Testing/setup-environment.php
+++ _Testing/setup-environment.php
@@ -1,10 +1,12 @@
 <?php
-// setup helpful constants
-require '../StandardConstants.php';
-
 // locate the path to the "include path"
-$path = pathinfo(pathinfo(pathinfo(__FILE__, PATHINFO_DIRNAME), PATHINFO_DIRNAME), PATHINFO_DIRNAME);
+$path = dirname(dirname(__FILE__));
+
+// setup helpful constants
+require $path.'/StandardConstants.php';
 
 // setup the auto-loader
+// step the path up one more hop to allowe referencing with the class prefix
+$path = dirname($path);
 require_once $path.'/ELSWAK/ClassLoader.php';
 $cal = new ELSWAK_ClassLoader(false, $path);
