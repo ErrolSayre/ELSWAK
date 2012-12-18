2f7842cba096d06ba1f352c7c997fc75a732d5eb
Errol Sayre <esayre@olemiss.edu>
2012-12-18 15:01:18 -0600

    Add dummy class to aid in testing class loader

---
+++ ClassLoaderDummy.php
@@ -0,0 +1,11 @@
+<?php
+/*
+	ELSWAK ClassLoaderDummy
+	
+	This class exists specifcally to test the class loading capabilities of the class loader within the PHPUnit tests.
+*/
+class ELSWAK_ClassLoaderDummy {
+	public function sayHello() {
+		return 'Hello';
+	}
+}
---
+++ _Testing/ClassLoaderZTest.php
@@ -0,0 +1,11 @@
+<?php
+// Provides code coverage for the class loader dummy class. The file is named with a Z to ensure it is loaded after the class loader tests so they can make proper use of loading the dummy class.
+require_once 'setup-environment.php';
+
+class ELSWAK_ClassLoaderDummyTest
+	extends PHPUnit_Framework_TestCase {
+	public function testHello() {
+		$var = new ELSWAK_ClassLoaderDummy;
+		$this->assertEquals('Hello', $var->sayHello());
+	}
+}
