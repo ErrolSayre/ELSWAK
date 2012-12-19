d7076660fb33e20bad87e156ceb94ba6b36025c0
Errol Sayre <esayre@olemiss.edu>
2012-12-19 09:38:07 -0600

    Create initial test for settable

---
+++ _Testing/SettableTest.php
@@ -0,0 +1,10 @@
+<?php
+require_once 'setup-environment.php';
+
+class ELSWAK_SettableTest
+	extends PHPUnit_Framework_TestCase {
+	
+	public function testConstructor() {
+		$var = new ELSWAK_Settable;
+	}
+}
