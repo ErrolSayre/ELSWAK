3d22ad5ad5415f6784d3d777493915f6c1fff102
Errol Sayre <esayre@olemiss.edu>
2013-02-13 01:13:01 -0600

    Add further tests

--- _Testing/Postal/AddressTest.php
+++ _Testing/Postal/AddressTest.php
@@ -2,20 +2,22 @@
 class ELSWAK_Postal_AddressTest
 	extends PHPUnit_Framework_TestCase {
 
-	public function testConstructor() {
+	public function testAddress() {
 		$address = new ELSWAK_Postal_Address;
 		$this->assertInstanceOf('ELSWAK_Postal_Address', $address);
+		$this->assertEmpty($address->cityStateZipLine);
 		
 		$address = new ELSWAK_Postal_Address(array(
 			'line1' => '30 Sorority Row',
 			'line2' => null,
 			'city' => 'University',
 			'state' => 'MS',
-			'postal' => '38677',
+			'zip' => 38677,
 			'country' => 'U.S.A'
 		));
 		$this->assertInstanceOf('ELSWAK_Postal_Address', $address);
 		$this->assertEquals(1, $address->lineCount);
+		$this->assertEquals('30 Sorority Row, University, MS 38677, U.S.A', $address->address('single-line'));
 		
 		$text = 'Office of Research'.LF
 			.'125 Old Chemistry'.LF
@@ -24,6 +26,7 @@ class ELSWAK_Postal_AddressTest
 		$this->assertInstanceOf('ELSWAK_Postal_Address', $address);
 		$this->assertEquals($text, $address->address);
 		$this->assertEquals(38677, $address->zipCode);
+		$this->assertEquals(3, count($address->address('array')));
 		
 		$text = 'Office of Information Technology 100 Powers Hall University Mississippi 38677-1848';
 		$address = $address->parseAddress($text);
@@ -32,5 +35,40 @@ class ELSWAK_Postal_AddressTest
 		$text = 'Office of Information Technology 100 Powers Hall University';
 		$address = $address->parseAddress($text);
 		$this->assertEquals('University', $address->city);
+		
+		$address = new ELSWAK_Postal_Address('Marty McFly', '9303 Lyon Drive', 'Hill Valley', 'California', 91331, 'U.S.A');
+		$this->assertEquals('9303 Lyon Drive', $address->line2);
+		$address->state = null;
+		$this->assertEquals('Hill Valley 91331', $address->cityStateZipLine);
+		$this->assertEquals(91331, $address->postalCode);
+		$address->postal = null;
+		$this->assertEquals('Hill Valley', $address->cityStateZipLine);
+		$address->state = 'CA';
+		$this->assertEquals('Hill Valley, CA', $address->cityStateZipLine);
+		
+		// do some obscure tests for coverage
+		$this->assertEquals(count($address->lines), $address->linesCopy->count());
+		$this->assertEmpty($address->line(3));
+		$address->line1 = null;
+		$this->assertEmpty($address->line1);
+		$address->line2 = null;
+		$this->assertEmpty($address->line2);
+		
+		$address = new ELSWAK_Postal_Address;
+		$address->line1 = 'Errol Sayre';
+		$address->line2 = '217 Powers Hall';
+		$this->assertEquals('Errol Sayre, 217 Powers Hall', "$address");
+		
+		$address->setStates(new ELSWAK_Array(array(
+			'Burgenland',
+			'Kärnten',
+			'Niederösterreich',
+			'Oberösterreich',
+			'Salzburg',
+			'Steiermark',
+			'Tirol',
+			'Vorarlberg',
+			'Vienna',
+		)));
 	}
 }
