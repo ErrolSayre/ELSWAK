fb614613f736fbc7eca06f57b9608915c15f7b2a
Errol Sayre <esayre@olemiss.edu>
2013-02-16 10:31:30 -0600

    Alias value methods

--- Integer.php
+++ Integer.php
@@ -20,15 +20,48 @@ class ELSWAK_Integer {
 	public function __toString() {
 		return $this->value;
 	}
-	
+
+
+
+//!Static methods
+	/**
+	 * Translate value to integer
+	 *
+	 * @param mixed $value
+	 * @return integer
+	 */
 	public static function integerForValue($value) {
 		return intval($value);
 	}
+	/**
+	 * Alias method for alternative naming convention
+	 *
+	 * @param mixed $value
+	 * @return integer
+	 */
+	public static function valueAsInteger($value) {
+		return self::integerForValue($value);
+	}
+	/**
+	 * Translate value to a non-negative integer
+	 *
+	 * @param mixed $value
+	 * @return integer Non-negative value
+	 */
 	public static function positiveIntegerForValue($value) {
 		$int = self::integerForValue($value);
 		if ($int >= 0) {
 			return $int;
 		}
 		return 0;
+	}
+	/**
+	 * Alias method for alternative naming convention
+	 *
+	 * @param mixed $value
+	 * @return integer Non-negative value
+	 */
+	public static function valueAsPositiveInteger($value) {
+		return self::postitiveIntegerForValue($value);
 	}
 }
