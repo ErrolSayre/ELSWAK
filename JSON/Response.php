1b1d3497ee70ea204bbd758a744264f8770e8ca5
Errol Sayre <esayre@olemiss.edu>
2013-04-17 15:30:18 -0500

    Add JSON encoding options
    This is simultaneously to support Pretty JSON but also to support other
    options a user may wish to use.

--- JSON/Response.php
+++ JSON/Response.php
@@ -1,10 +1,38 @@
 <?php
+//!Stub Constants
+if (!defined('JSON_PRETTY_PRINT')) {
+	define('JSON_PRETTY_PRINT', 0);
+}
+
+
+
 class ELSWAK_JSON_Response
 	extends ELSWAK_HTTP_Response {
-	public function __construct() {
+	
+	protected $jsonEncodeOptions;
+
+
+
+	public function __construct($prettyJson = false) {
 		$this->setContentType('text/javascript');
 		$this->setStatus('OK');
+		$this->setPrettyJSON($prettyJson);
 	}
+
+
+
+	public function setJSONEncodeOptions($value) {
+		$this->jsonEncodeOptions = (integer) $value;
+	}
+	public function setPrettyJSON($value) {
+		if (ELSWAK_Boolean::valueAsBoolean($value)) {
+			$this->jsonEncodeOptions = JSON_PRETTY_PRINT;
+		}
+		return $this;
+	}
+
+
+
 	protected function filterContentByType($content, $type) {
 		// determine if the content is a string
 		if (is_string($content) && strtolower($type) == 'json') {
@@ -14,14 +42,14 @@ class ELSWAK_JSON_Response
 			}
 		}
 		// since the content is not an acceptable JSON string, encode it
-		return json_encode($content);
+		return json_encode($content, $this->jsonEncodeOptions);
 	}
 	public function content() {
 		// construct the json representation of this response
 		$json = '';
 		$json .= '"status":"'.$this->status.'",';
 		if (count($this->messages))
-			$json .= '"messages":'.json_encode($this->messages).',';
+			$json .= '"messages":'.json_encode($this->messages, $this->jsonEncodeOptions).',';
 		else
 			$json .= '"messages":null,';
 		if (count($this->body)) {
