5017d409a1cff02e4eab098a244e7254d1d0c79d
Errol Sayre <esayre@olemiss.edu>
2013-05-01 13:48:20 -0500

    Create gettable wrapper
    This class allows scalar variables (and stringifiable arrays and
    objects) to be wrapped for sending to an item expecting a "gettable".

---
+++ Gettable/Wrapper.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Wrap a value in a gettable object
+ */
+class ELSWAK_Gettable_Wrapper
+	implements ELSWAK_Gettable {
+
+	protected $value;
+	
+	public function __construct($value) {
+		if (is_array($value)) {
+			$value = implode(', ', $value);
+		}
+		elseif (is_object($value)) {
+			if (method_exists($value, '__toString')) {
+				$value = (string) $value;
+			}
+			else {
+				$value = '';
+			}
+		}
+		$this->value = (string) $value;
+	}
+	public function get($property) {
+		return $this->value;
+	}
+	public function __toString() {
+		return $this->value;
+	}
+}
