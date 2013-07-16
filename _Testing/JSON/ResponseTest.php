fca4372ea6a5eae0c09fa712193dfdcd1fbd9216
Errol Sayre <esayre@olemiss.edu>
2013-07-16 10:57:47 -0500

    Setup baseline JSON response tests

---
+++ _Testing/JSON/ResponseTest.php
@@ -0,0 +1,23 @@
+<?php
+class ELSWAK_JSON_ResponseTest
+	extends PHPUnit_Framework_TestCase {
+	
+	public function testSimpleResponse() {
+		$response = new ELSWAK_JSON_Response;
+		
+		$response->addContent('item 1');
+		$response->addContent('item 2');
+		$response->addContent('first', 'firstName');
+		$response->addContent('last', 'lastName');
+		$response->addContent('{"firstName":"person","lastName":"1"}', 'json string 1');
+		$response->addContent('{"firstName":"person","lastName":"2"}', 'json literal 1', 'json');
+		$response->addContent(array('first' => 'Errol', 'last' => 'Sayre'), 'myself');
+		
+		// write the response to a file
+		$path = pathinfo(__FILE__, PATHINFO_DIRNAME).'/'.pathinfo(__FILE__, PATHINFO_FILENAME).'-simple.';
+		file_put_contents($path.'actual.txt', $response->debugOutput());
+		
+		// compare the written response to the expected
+		$this->assertFileEquals($path.'expected.txt', $path.'actual.txt');
+	}
+}
