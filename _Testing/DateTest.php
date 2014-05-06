d0fad1f2647a9d9e927c77ed66684edcadc3393a
Errol Sayre <esayre@olemiss.edu>
2014-05-06 11:16:03 -0500

    Update tests to reflect change in COOKIE format
    See PHP bug https://bugs.php.net/bug.php?id=53879

--- _Testing/DateTest.php
+++ _Testing/DateTest.php
@@ -5,10 +5,10 @@ class ELSWAK_DateTest
 	public function testDate() {
 		$tests = array(
 			    'ATOM' => '2013-11-11T11:11:11+00:00',
-			  'COOKIE' => 'Monday, 11-Nov-13 11:11:11 UTC',
+			  'COOKIE' => 'Monday, 11-Nov-2013 11:11:11 UTC',
 			'DATETIME' => '2013-11-11 11:11:11',
 			    'FULL' => 'Monday, November 11, 2013',
-			'FULLTIME' => '11:11:11 am',
+			'FULLTIME' => '11:11:11 am UTC',
 			 'ISO8601' => '2013-11-11T11:11:11+0000',
 			  'RFC822' => 'Mon, 11 Nov 13 11:11:11 +0000',
 			  'RFC850' => 'Monday, 11-Nov-13 11:11:11 UTC',
